407ee9f90cba910c763c466da96736d977a052eb
bussracicekk <37375743+bussracicekk@users.noreply.github.com>
2019-01-18 15:59:45 +0300

    Update main.c

--- main.c
+++ main.c
@@ -5,6 +5,8 @@ int main()
 {
     accountOperations();
 }
+
+//************you can make any operation about your bank account in this method**********************//
 void accountOperations(){
 
     char username[5];
@@ -21,6 +23,7 @@ void accountOperations(){
     printf("Options:\n1:Sign In\n2:Create a new user\n");
     printf("Please, sign in if you have an account in our bank, or create a new account if you do not have an account in our bank\n");
     scanf("%d",&option);
+
     switch(option){
         case 1:
             printf("Enter your username: ");
@@ -32,44 +35,21 @@ void accountOperations(){
                     for(int j = 0; j < 6; j++){
                         if(passwords[j] == password){
                             printf("You logged in");
-                            printf("Options\n1:Lodgement\n2:Withdrawal\n3:Transfer\n4:Cash Learning\n5:Card Refund\n");
+                            printf("Options\n1:Lodgement\n2:Withdrawal\n3:Transfer\n4:Cash Learning\n5:Card Refund\n6:Return Home Page\n");
                             printf("\nPlease, Choose an option: ");
                             scanf("%d",&option1);
 
                             switch(option1){
                                 case 1:
-                                    printf("Your Balance:  %d",cash);
-                                    printf("\nPlease, Enter a price: ");
-                                    scanf("%d",&price);
-                                    if(price > cash){
-                                        printf("\nCash is not enough\n");
-                                    }
-                                    else{
-                                        cash -= price;
-                                        printf("\nYour rest cash: %d\n",cash);
-                                    }
+                                    lodgement(cash, price);
                                     break;
 
                                 case 2:
-                                    printf("\nYour Cash:  %d\n",cash);
-                                    printf("Please, Enter a price: ");
-                                    scanf("%d",&price);
-
-                                    cash += price;
-                                    printf("\nCash: %d\n",cash);
+                                    withdrawal(cash, price);
                                     break;
 
                                 case 3:
-                                    printf("\nYour Cash:  %d\n",cash);
-                                    printf("Please, Enter a price for transfer: ");
-                                    scanf("%d",&price);
-                                    if(price > cash){
-                                        printf("\nCash is not enough\n");
-                                    }
-                                    else{
-                                        cash -= price;
-                                        printf("\nYour rest cash: %d\n",cash);
-                                    }
+                                    transfer(cash, price);
                                     break;
 
                                 case 4:
@@ -79,6 +59,9 @@ void accountOperations(){
                                 case 5:
                                     printf("\nCard was returned.\n");
                                     break;
+
+                                
+                                //****************default option is used for wrong choice**********************//
                                 default:
                                     printf("Request is not understood.\nPlease, choose an option\n");
                                     printf("Options\n1:Lodgement\n2:Withdrawal\n3:Transfer\n4:Cash Learning\n5:Card Refund\n");
@@ -87,15 +70,19 @@ void accountOperations(){
                         }
                     }
                 }
+                
+                //***********if usernames array does not have your username, here is works**************//
                 else{
                     printf("username is not correct");
                 }
             }
             break;
+
+        //***************Create new user**********************//
         case 2:
             printf("Please create a username: ");
             scanf("%s",&usernamenew);
-            usernames[6]==usernamenew;
+            usernames[6]==usernamenew;//new username is added into usernames array
             printf("Please create a password: ");
             scanf("%d",&password1);
             passwords[5]==password1;
@@ -103,12 +90,56 @@ void accountOperations(){
             accountOperations();
             break;
 
+        //****************default option is used for wrong choice**********************//
         default :
             printf("Request is not understood.\nPlease, choose an option\n");
             printf("Options:\n1:Sign In\n2:Create a new user\n");
             scanf("%d",&option);
+            break;
 
     }
 
     return 0;
+}
+
+//************lodgement operation*******************//
+void lodgement(int cash, int price){
+     printf("Your Balance:  %d",cash);
+     printf("\nPlease, Enter a price: ");
+     scanf("%d",&price);
+     
+     //********if you have not enough cash, here is works*********//
+     if(price > cash){
+         printf("\nCash is not enough\n");
+     }
+     
+     //********if you have enough cash, here is works************//
+     else{
+         cash -= price;
+         printf("\nYour rest cash: %d\n",cash);
+     }
+}
+
+//************withdrawal operation*******************//
+void withdrawal(int cash, int price){
+    printf("\nYour Cash:  %d\n",cash);
+    printf("Please, Enter a price: ");
+    scanf("%d",&price);
+
+    cash += price;
+    printf("\nCash: %d\n",cash);
+}
+
+//************transfer operation*******************//
+void transfer(int cash, int price){
+    printf("\nYour Cash:  %d\n",cash);
+    printf("Please, Enter a price for transfer: ");
+    scanf("%d",&price);
+    if(price > cash){
+        printf("\nCash is not enough\n");
+    }
+    else{
+        cash -= price;
+        printf("\nYour rest cash: %d\n",cash);
+    }
 }
